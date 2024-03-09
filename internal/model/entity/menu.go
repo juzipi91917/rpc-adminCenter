cb0c7f223fed2e21d24c923caf7510c8f72a782d
yuqi <43877572+yuqir@users.noreply.github.com>
2024-03-09 11:03:55 +0800

    test

--- internal/model/entity/menu.go
+++ internal/model/entity/menu.go
@@ -8,6 +8,7 @@ type (
 		UpdatedAt *int64 `gorm:"column:updated_at;type:bigint not null;default:0;comment:更新时间"`                      // 更新时间
 		State     *int32 `gorm:"column:state;type:tinyint(4) not null;default:0;comment:状态 0禁用 1启用"`
 		Name      string `gorm:"column:name;type:varchar(255) not null;default:'';comment:菜单名称"`
+		Route     string `gorm:"column:route;type:varchar(255) not null;default:'';comment:前端菜单路由"`
 		PId       int64  `gorm:"column:p_id;type:bigint not null;default:0;comment:父级菜单id"`
 		LevelIds  string `gorm:"column:level_ids;type:varchar(1500) not null;default:'';comment:父级菜单层级id"`
 		Remarks   string `gorm:"column:remarks;type:varchar(255) not null;default:'';comment:备注"`
